626805bea0533d1116f593eb72e9ffa2ee8db066
Ze Felipe <felipe.paula1903@gmail.com>
2022-12-15 01:13:53 -0300

    estou quase terminando o projeto

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +26,7 @@ SECRET_KEY = 'django-insecure-i2j2_((-4&#p69i_x-!s83t5917y!e5%t7i6213zzyk8)77f8y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['zefelipe-ce.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -142,4 +141,3 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-django_on_heroku.settings(locals())
